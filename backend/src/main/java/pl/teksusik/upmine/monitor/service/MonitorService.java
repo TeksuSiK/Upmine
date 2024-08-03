5bbb17761336f26e3b1cec8f318d163908600f70
TeksuSiK <oskar@teksusik.pl>
2024-08-03 18:45:09 +0200

    Add ping availability checker implementation to availability checkers map in monitor service

--- backend/src/main/java/pl/teksusik/upmine/monitor/service/MonitorService.java
+++ backend/src/main/java/pl/teksusik/upmine/monitor/service/MonitorService.java
@@ -2,6 +2,7 @@ package pl.teksusik.upmine.monitor.service;
 
 import pl.teksusik.upmine.availability.AvailabilityChecker;
 import pl.teksusik.upmine.availability.http.HttpAvailabilityChecker;
+import pl.teksusik.upmine.availability.ping.PingAvailabilityChecker;
 import pl.teksusik.upmine.heartbeat.Heartbeat;
 import pl.teksusik.upmine.monitor.Monitor;
 import pl.teksusik.upmine.monitor.MonitorType;
@@ -11,7 +12,8 @@ import java.util.Map;
 
 public class MonitorService {
     private final Map<MonitorType, AvailabilityChecker> availabilityCheckers = new HashMap<>(Map.of(
-            MonitorType.HTTP, new HttpAvailabilityChecker()
+            MonitorType.HTTP, new HttpAvailabilityChecker(),
+            MonitorType.PING, new PingAvailabilityChecker()
     ));
 
     public Heartbeat checkAvailability(Monitor monitor) {
