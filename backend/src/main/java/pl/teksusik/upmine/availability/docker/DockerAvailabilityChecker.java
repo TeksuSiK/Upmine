77b08cf3e31fc7a303975344ac42d817f7bd1ae5
TeksuSiK <oskar@teksusik.pl>
2024-08-14 19:37:17 +0200

    Catch NotFoundException if container does not exists

--- backend/src/main/java/pl/teksusik/upmine/availability/docker/DockerAvailabilityChecker.java
+++ backend/src/main/java/pl/teksusik/upmine/availability/docker/DockerAvailabilityChecker.java
@@ -3,6 +3,9 @@ package pl.teksusik.upmine.availability.docker;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.InspectContainerCmd;
 import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.teksusik.upmine.availability.AvailabilityChecker;
 import pl.teksusik.upmine.docker.service.DockerHostService;
 import pl.teksusik.upmine.heartbeat.Heartbeat;
@@ -11,6 +14,8 @@ import pl.teksusik.upmine.monitor.Monitor;
 import pl.teksusik.upmine.monitor.docker.DockerMonitor;
 
 public class DockerAvailabilityChecker implements AvailabilityChecker {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DockerAvailabilityChecker.class);
+
     private final DockerHostService dockerHostService;
 
     public DockerAvailabilityChecker(DockerHostService dockerHostService) {
@@ -23,7 +28,13 @@ public class DockerAvailabilityChecker implements AvailabilityChecker {
 
         DockerClient dockerClient = this.dockerHostService.getClient(dockerMonitor.getDockerHost());
         InspectContainerCmd inspectCommand = dockerClient.inspectContainerCmd(dockerMonitor.getDockerContainerId());
-        InspectContainerResponse inspectResponse = inspectCommand.exec();
+        InspectContainerResponse inspectResponse;
+        try {
+            inspectResponse = inspectCommand.exec();
+        } catch (NotFoundException exception) {
+            LOGGER.error("An error occurred while checking availability", exception);
+            return HeartbeatFactory.notAvailable(exception.getMessage());
+        }
 
         boolean running = Boolean.TRUE.equals(inspectResponse.getState().getRunning());
         if (running) {
